cc773fcacd60157ca72757fdf0bf99b23d357155
Juan Andrés Parra Muñoz <25157583+metalnerd777@users.noreply.github.com>
2024-06-20 01:14:35 -0400

    wip map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,9 +1,16 @@
-import Mapbox, {MapView} from '@rnmapbox/maps';
+import Mapbox, {Camera, LocationPuck, MapView} from '@rnmapbox/maps';
 //public token
 const accessToken =
   'pk.eyJ1IjoianVhbnBhcnJhMTk4OCIsImEiOiJjbHhkeml4Z2owYmRzMm1wdDdxaW1yYXp6In0.oy9iRkyNdufQYpX4X8B6Xg';
 Mapbox.setAccessToken(accessToken);
 
 export default function Map() {
-  return <MapView style={{flex: 1}} />;
+  return (
+    <MapView
+      style={{flex: 1}}
+      styleURL="mapbox://styles/mapbox/navigation-night-v1">
+      <Camera followUserLocation />
+      <LocationPuck />
+    </MapView>
+  );
 }
