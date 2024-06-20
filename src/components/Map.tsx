6151c0b2a9db42c1a1c3318647b03e4c350e9583
Juan Andrés Parra Muñoz <25157583+metalnerd777@users.noreply.github.com>
2024-06-20 02:00:11 -0400

    localizacion

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,8 +1,12 @@
 import Mapbox, {Camera, LocationPuck, MapView} from '@rnmapbox/maps';
+import Geolocation from '@react-native-community/geolocation';
+
 //public token
 const accessToken =
   'pk.eyJ1IjoianVhbnBhcnJhMTk4OCIsImEiOiJjbHhkeml4Z2owYmRzMm1wdDdxaW1yYXp6In0.oy9iRkyNdufQYpX4X8B6Xg';
 Mapbox.setAccessToken(accessToken);
+
+Geolocation.getCurrentPosition(info => console.log(info));
 
 export default function Map() {
   return (
